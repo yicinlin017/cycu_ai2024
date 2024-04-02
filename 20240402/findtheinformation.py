29aa1360e1b81fdaf6c776136d9e4b2cd78f2da3
yicinlin017 <160599446+yicinlin017@users.noreply.github.com>
2024-04-02 11:35:35 +0000

    04021

--- 20240402/findtheinformation.py
+++ 20240402/findtheinformation.py
@@ -1,8 +1,8 @@
-#讀取目錄內的XML 將文字結構<title>的內容中找出其所屬縣市 例如:基隆市 臺北市 新北市 桃園市 新竹市 新竹縣 苗栗縣 台中市 南投縣 彰化縣 雲林縣 嘉義縣 嘉義市 臺南市 高雄市 屏東縣 
-#然後再將文字結構<descripion>內容的 04/02 晚上溫度為多少 例如:04/02 晚上 溫度:22 ~ 27
-#將這些資訊打印出來
-
+#讀取目錄內的XML 將文字結構<title>的內容中找出其縣市 例如:基隆市 臺北市 新北市 桃園市 新竹市 新竹縣 苗栗縣 台中市 南投縣 彰化縣 雲林縣 嘉義縣 嘉義市 臺南市 高雄市 屏東縣 設定為縣市欄位
+#然後再找出文字結構<descripion>內容需有 04/02 晚上溫度 字樣的內容找出 例如:04/02 晚上 溫度:22 ~ 27 設定為溫度欄位
+#將縣市欄位及溫度欄位的內容打印出來
 import os
+import re
 from xml.etree import ElementTree
 
 # 讀取目錄內的XML檔案
@@ -12,11 +12,18 @@ for file_name in os.listdir():
         tree = ElementTree.parse(file_name)
         root = tree.getroot()
         
-        # 找到所有的item
-        for item in root.findall('.//item'):
-            # 找到title和description
+        # 找到所有的<item>元素
+        for item in root.findall('channel/item'):
             title = item.find('title').text
             description = item.find('description').text
             
-            # 打印縣市和溫度
-            print(title, description)
+            # 檢查是否為04/02晚上的描述
+            if '04/02 晚上 溫度' in description:
+                # 使用正則表達式找到溫度
+                match = re.search(r'04/02 晚上 溫度 (\d+)', description)
+                if match:
+                    temperature = match.group(1)
+                    # 打印縣市和溫度
+                    print('縣市:', title)
+                    print('04/02 晚上溫度:', temperature)
+                    print()
---
+++ 20240402/taiwanprint.py
@@ -0,0 +1,23 @@
+#尋找台灣的區界地圖 shape file/ geojson file
+#https://data.gov.tw/dataset/7441
+
+#read 20240402/tract_20140313.json as geopanas
+import geopandas as gpd
+import pandas as pd
+
+#read shape file
+taiwan=gpd.read_file('/workspaces/cycu_ai2024/20240402/mapdata202301070205/COUNTY_MOI_1090820.shp')
+import os
+#print cwd
+print (taiwan)
+
+#plot taiwan using matplotlib
+import matplotlib.pyplot as plt
+
+#輸出台灣地圖的時候 圖面顯示的範圍 緯度最低為21.5度 緯度最高為25.5度 經度最低為119度 經度最高為122度
+taiwan.plot()
+plt.xlim(119,122)
+plt.ylim(21.5,25.5)
+plt.show()
+#save to png file
+plt.savefig('20240402/taiwan_map.png')
