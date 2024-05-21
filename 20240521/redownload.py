a276dcb3b371f0d7102254e707bd970cff6895f0
yicinlin017 <g11272006@cycu.edu.tw>
2024-05-21 20:41:12 +0800

    0521

--- 20240521/redownload.py
+++ 20240521/redownload.py
@@ -1,30 +1,51 @@
-#爬蟲https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/ 
-#下載名稱為M05A_2024MMDD.tar.gz的檔案 
-#MM只採用01 02 03 04 DD只採用01 02 03 04 05 06 07 08 09 10 11 12 13 14 15 16 17 18 19 20 21 22 23 24 25 26 27 28 29 30 31
-#存儲在/workspaces/cycu_ai2024/20240521/imfotmatiom 資料夾下
+#爬蟲https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/
+#下載網址內檔案名稱 M05A_2024MMDD.tar.gz 01<=MM<=04 01<=DD<=31
+#儲存在/workspaces/cycu_ai2024/20240521/imfotmatiom 資料夾下
+
 import requests
 import os
 import time
-import calendar
+import datetime
+import random
+import sys
+import logging
 
-url = "https://tisvcloud.freeway.gov.tw/history/TDCS/M05A/"
-path = "/workspaces/cycu_ai2024/20240521/imfotmatiom"
-for i in range(1, 5):
-    _, days_in_month = calendar.monthrange(2024, i)
-    for j in range(1, days_in_month + 1):
-        if i < 10:
-            month = "0" + str(i)
-        else:
-            month = str(i)
-        if j < 10:
-            day = "0" + str(j)
-        else:
-            day = str(j)
-        file_name = "M05A_2024" + month + day + ".tar.gz"
-        print(file_name)
-        response = requests.get(url + month + day + ".tar.gz", timeout=60)
-        print(response.status_code)
-        with open(os.path.join(path, file_name), "wb") as f:
-            f.write(response.content)
-        time.sleep(1)
-print("Download finish")
+def download_file(url, filename):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(filename,
+                    'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    return filename
+
+def download_file_to_folder(url, folder):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(os.path.join(folder, os.path.basename(url)),
+                    'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    return os.path.join(folder, os.path.basename(url))
+
+def download_file_to_folder_with_filename(url, folder, filename):
+    # NOTE the stream=True parameter below
+    with requests.get(url, stream=True) as r:
+        r.raise_for_status()
+        with open(os.path.join(folder, filename),
+                    'wb') as f:
+                for chunk in r.iter_content(chunk_size=8192):
+                    f.write(chunk)
+    return os.path.join(folder, filename)
+
+def download_file_to_folder_with_filename_and_retry(url, folder, filename, retry=3):
+    for i in range(retry):
+        try:
+            return download_file_to_folder_with_filename(url, folder, filename)
+        except Exception as e:
+            logging.error(f"Error: {e}")
+            time.sleep(random.randint(1, 3))
+    return None
+
