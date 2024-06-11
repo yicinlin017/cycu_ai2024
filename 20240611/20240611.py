b375ed462661591470b273e87ef59bf1b8b747ba
yicinlin017 <g11272006@cycu.edu.tw>
2024-06-11 18:31:13 +0800

    20240611

---
+++ 20240611/20240611.py
@@ -0,0 +1,4 @@
+ rd = requests.post(
+    f"https://notify-api.line.me/api/notify",
+     headers={"Authorization": f"Bearer {IVI7tG0vSu7TAwkr175RJ4iVE2OV8PV0vXR5TlF59q0}"},
+    data={"message": f"{林意芹}"})
