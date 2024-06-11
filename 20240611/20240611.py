d959f15117e1dda72e409c7c28c000240c6d4df4
yicinlin017 <g11272006@cycu.edu.tw>
2024-06-11 18:40:55 +0800

    1

--- 20240611/20240611.py
+++ 20240611/20240611.py
@@ -1,4 +1,4 @@
- rd = requests.post(
-    f"https://notify-api.line.me/api/notify",
-     headers={"Authorization": f"Bearer {9lWfhQcoUhVnLxaMT1wpIoN44WUnrXy8hy4sy6wENDu}"},
-    data={"message": f"{林意芹}"})
+    rd = requests.post(
+        f"https://notify-api.line.me/api/notify",
+        headers={"Authorization": f"Bearer {F4kGx7JGeGKQgOmkXOgLYRGjNwjpN0o2c4fXE9zxNdD}"},
+        data={"message": f"{林意芹}"})
